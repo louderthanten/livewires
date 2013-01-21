925f53412fd03f5263e856c2052cced55b6cf84e
Travis Gertz <trav@louderthan10.com>
2013-01-21 00:30:38 -0800

    Do a bunch of renaming

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -1,12 +1,47 @@
-<?php include($_SERVER['DOCUMENT_ROOT'].'/_inc/html_head.php'); ?>
-    <body class="home-pg">
-        <?php include($_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'); ?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Live Wires</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="/css/livewires.css">
+</head>
+<body class="home-pg">
+    <header id="header" class="container">
+        
+        <h1><a href="/" title="">Logo</a></h1>
+        
+        <nav class="search">
+            <form action="/search/results.php" method="get">
+                <input type="search" name="search" placeholder="search" />
+            </form>
+        </nav>
+        <nav class="primary-nav">
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="/news/">News</a></li>
+                <li><a href="">Products</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
 
-        <div class="main container" id="main">
-	      	<figure class="hero">
-	        </figure>
-	     
-             
-        </div>
+    </header><!-- /header -->
 
-<?php include($_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'); ?>
+    <div class="main container" id="main">
+      	
+        <figure>
+            <img src="http://placehold.it/2000x800&text=Hero">
+        </figure>
+      
+    </div>
+
+    <footer id="footer" class="container">
+        <small>This project is <a href="http://louderthan10.com">Louder Than Ten</a>.</small>
+    </footer>
+
+    <script src="/js/vendor/jquery-1.8.3.min.js"></script>
+    <script src="/js/prototype.js"></script>
+
+</body>
+</html>
