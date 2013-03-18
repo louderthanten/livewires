41b2398f00f171a64bed81312fd5f7be04829b9a
Travis Gertz <trav@louderthan10.com>
2013-03-18 10:33:43 -0700

    Remove leftover includes.

--- httpdocs/_inc/footer.php
+++
@@ -1,9 +0,0 @@
-        <footer id="footer" class="container">
-
-        </footer>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
-        
-        <script src="/js/prototype.js"></script>
-    </body>
-</html>
