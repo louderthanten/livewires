9c1759673ead718efac7550f6d632c4e6eaffaf0
Travis Gertz <trav@louderthan10.com>
2013-05-14 02:11:23 -0700

    Cleanup and nearly finish design

--- config.rb
+++ config.rb
@@ -7,8 +7,8 @@ sass_dir = "source/scss"
 images_dir = "images"
 javascripts_dir = "js"
 
-output_style = :expanded
-environment = :development
+output_style = :compressed
+environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
